1128d0ed95463616738655073097f08a3fdf6304
Adam Jamil <adam.a1d9c@gmail.com>
2022-05-30 21:32:09 -0400

    Update matrix_utils.py

--- matrix_utils.py
+++ matrix_utils.py
@@ -1,6 +1,7 @@
 from typing import List, TypeVar, Optional, Callable
+from polynomial import Polynomial
 
-from expression import *
+from re_sum import *
 from states import get_states
 
 
@@ -15,7 +16,7 @@ def generate_transition_matrix(n: int) -> List[List[RESum]]:
             tot = cp(zero_poly)
             for dst in range(3):
                 n_c = x[dst] - (src == dst)
-                add_poly(tot, monomial(n_c))
+                tot += Polynomial.monomial(n_c)
             # mult_poly_scalar(tot, [n, n_c])
             # print(f"tot: {fmt_polynomial(tot)}, state: {x}")
             for dst in range(3):
@@ -24,29 +25,34 @@ def generate_transition_matrix(n: int) -> List[List[RESum]]:
                 y = y[0], max(y[1], y[2]), min(y[1], y[2])
                 # print(f"dst: {dst}, y: {y}")
                 idx_y = states.index(y)
-                add_REsum(A[idx_x][idx_y], [(mult_poly_scalar(monomial(n_c), [x[src], n]), tot)])
+                A[idx_x][idx_y] += RationalFunc(Polynomial.monomial(n_c) * Rational(x[src], n), cp(tot))
 
     return A
 
 
 def evaluate_matrix_at_rational(A: List[List[RESum]], lambd: Rational):
-    return [[evaluate_REsum_at_rat(f, lambd) for f in row] for row in A]
+    return [[f(lambd) for f in row] for row in A]
 
 
 T = TypeVar("T")
+import time
 
 
-def mult_matrix(A: List[List[T]], B: List[List[T]], add: Optional[Callable[[T, T], T]] = None,
-                mult: Optional[Callable[[T, T], T]] = None, add_id: Optional[T] = None):
-    add = add or (lambda x, y: x + y)
-    mult = mult or (lambda x, y: x * y)
-    add_id = 0 if not add_id else add_id
+def mult_matrix(A: List[List[T]], B: List[List[T]], add_id: Optional[T] = None):
+    add_id = 0 if add_id is None else add_id
     n, p, m = len(A), len(A[0]), len(B[0])
     C = [[cp(add_id) for _ in range(n)] for _ in range(m)]
+    mul_time = 0
+    add_time = 0
     for i in range(n):
         for j in range(p):
             for k in range(m):
-                C[i][k] = add(C[i][k], mult(A[i][j], B[j][k]))
+                start = time.perf_counter_ns()
+                res = A[i][j] * B[j][k]
+                mul_time += time.perf_counter_ns() - start
+                start = time.perf_counter_ns()
+                C[i][k] += res
+                add_time += time.perf_counter_ns() - start
+    print(mul_time  / 1000000000)
+    print(add_time  / 1000000000)
     return C
-
-# def pow_matrix
